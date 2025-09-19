c7a729f04913dd18cc905ba382945793f58c4fad
Gaston Gonzalez <gascoar@gmail.com>
2025-09-19 15:52:33 -0300

    multi_hist: agregar labels

--- plot_scripts/multi_hist.py
+++ plot_scripts/multi_hist.py
@@ -4,25 +4,29 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
-if len(sys.argv) < 2:
+def uso():
+    print("multi_hist: <output file> <input file1> <label1> <input file2> <label2> <input file3> <label3>")
+
+
+arglen = len(sys.argv)
+if arglen < 2 or (arglen % 2):
+    uso()
     raise ValueError("Incorrect number of arguments")
 
 outfile = sys.argv[1]
-df_list = []
-x_axes = []
-y_axes = []
+l = []
 
-for i in range(2, len(sys.argv)):
-    df_list.append(pd.read_csv(sys.argv[i]))
-
-for df in df_list:
-    x_axes.append(df.iloc[:,0])
-    y_axes.append(df.iloc[:, 1])
+for i in range(2, len(sys.argv), 2):
+    df = pd.read_csv(sys.argv[i])
+    label = sys.argv[i + 1]
+    l.append((df, label))
 
 plt.figure()
-x = x_axes[1]
-for y in y_axes:
-    plt.bar(x, y, width = 0.8)
+for t in l:
+    x = t[0].iloc[:, 0]
+    y = t[0].iloc[:, 1]
+    plt.bar(x, y, label = t[1], width = 0.8)
+    plt.legend(loc = 'best')
 plt.xlabel("Hamming Distance")
 plt.ylabel("Cuentas")
 plt.title("Histogram")
