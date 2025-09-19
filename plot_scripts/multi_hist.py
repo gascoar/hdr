2bf45d14d355a8789c76d3f152f49f92e3b72138
Gaston Gonzalez <gascoar@gmail.com>
2025-09-19 14:01:36 -0300

    multi_hist: generalizar numero de argumentos

--- plot_scripts/multi_hist.py
+++ plot_scripts/multi_hist.py
@@ -1,34 +1,31 @@
+#!/usr/bin/env python
+
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
-if sys.argv.len() < 2:
+if len(sys.argv) < 2:
+    raise ValueError("Incorrect number of arguments")
 
+outfile = sys.argv[1]
+df_list = []
+x_axes = []
+y_axes = []
 
-ifile1 = sys.argv[1]
-ifile2 = sys.argv[2]
-ifile3 = sys.argv[3]
-ifile4 = sys.argv[4]
-outfile = sys.argv[5]
+for i in range(2, len(sys.argv)):
+    df_list.append(pd.read_csv(sys.argv[i]))
 
-df1 = pd.read_csv(ifile1)
-df2 = pd.read_csv(ifile2)
-df3 = pd.read_csv(ifile3)
-df4 = pd.read_csv(ifile4)
-
-x = df1.iloc[:, 0]
-y1 = df1.iloc[:, 1]
-y2 = df2.iloc[:, 1]
-y3 = df3.iloc[:, 1]
-y4 = df4.iloc[:, 1]
+for df in df_list:
+    x_axes.append(df.iloc[:,0])
+    y_axes.append(df.iloc[:, 1])
 
 plt.figure()
-plt.bar(x, y1, width = 0.8)
-plt.bar(x, y2, width = 0.8)
-plt.bar(x, y3, width = 0.8)
-plt.bar(x, y4, width = 0.8)
+x = x_axes[1]
+for y in y_axes:
+    plt.bar(x, y, width = 0.8)
 plt.xlabel("Hamming Distance")
 plt.ylabel("Cuentas")
 plt.title("Histogram")
 plt.grid()
 plt.savefig(outfile)
+
