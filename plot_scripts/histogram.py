05366ad80fd5bacf731388ab9740b45b4457e3a2
Gaston Gonzalez <gascoar@gmail.com>
2026-02-16 10:28:08 -0300

    histogram.py: poner cantidad de DH evaluadas como subtitulo

--- plot_scripts/histogram.py
+++ plot_scripts/histogram.py
@@ -34,7 +34,6 @@ y_fit = Gauss(x, *params)
 end_x = mean * 2
 plt.plot(x, y, 'o', label='Data')
 plt.plot(x, y_fit, '-', label='Fit')
-plt.plot([], [], ' ', label=f"ndh = {nexp}")
 plt.xlim([0, end_x])
 plt.axvline(mean, color='green', linestyle='--', linewidth=2, label=f"Mean = {mu_fit:.3f}")
 plt.axvline(mu_fit + sigma_fit, color='orange', linestyle=':',
@@ -44,7 +43,7 @@ plt.axvline(mu_fit - sigma_fit, color='orange', linestyle=':',
             linewidth=2, label=f"μ - σ = {mu_fit - sigma_fit:.3f}")
 plt.xlabel("Hamming Distance")
 plt.ylabel("Tasa repeticion")
-plt.title("Histograma y ajuste")
+plt.title("Histograma y ajuste\n (ndh = %s)" %nexp)
 plt.legend()
 plt.grid()
 plt.savefig(outfile)
