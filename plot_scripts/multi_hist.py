95f09bfede1bd4df36f0db05d8394bbe35732506
Gaston Gonzalez <gascoar@gmail.com>
2026-03-10 19:26:27 -0300

    multi_hist: agregar estimacion gaussiana

--- plot_scripts/multi_hist.py
+++ plot_scripts/multi_hist.py
@@ -3,6 +3,9 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from gauss import gauss, normalize
 
 def uso():
     print("multi_hist: <output file> <input file1> <label1> <input file2> <label2> <input file3> <label3>")
@@ -25,13 +28,22 @@ plt.figure()
 for t in l:
     x = t[0].iloc[:, 0]
     y = t[0].iloc[:, 1] / t[0].iloc[:, 1].max()
+    y = y/np.max(y)
+    mean = np.average(x, weights = y)
+    std = np.sqrt(np.sum(y * (x - mean)**2) / np.sum(y))
+    p0 = [1, mean, std]
+    params, covariance = curve_fit(gauss, x, y, p0 = p0)
+    A_fit, mu_fit, sigma_fit = params
+    y_fit = gauss(x, *params)
+    plt.plot(x, y_fit, '-')#, label = "Fit " + t[1])
+    plt.plot(x, y, 'o', label = t[1])
+    plt.axvline(mean, color = 'black', linestyle='--', linewidth = 1, label = f"Mean = {mu_fit:.3f}")
+    plt.legend(loc = 'best', prop = {'size': 8})
 
-    plt.bar(x, y, label = t[1], width = 0.8)
-    plt.legend(loc = 'best')
-plt.xlim([0, 45])
+plt.xlim([0, 40])
 plt.xlabel("Hamming Distance")
 plt.ylabel("Cuentas")
-plt.title("Histogram")
+plt.title("Histograma y ajuste gaussiano")
 plt.grid()
 plt.savefig(outfile, dpi = 300)
 
